f4e4f694a77824d4c56fb73e5e8f7cc87752e99f
Dynee <lucaspadden@gmail.com>
2021-02-07 17:05:40 -0500

    working on tests

--- subreddit/tests.py
+++ subreddit/tests.py
@@ -9,14 +9,15 @@ import datetime
 class SubredditTestCase(TestCase):
     def setUp(self):
         self.user = User.objects.create('testUser', 'test@example.com', 'password')
-        self.subreddit = Subreddit.objects.create(name='Missing411')
+        self.subreddit = Subreddit.objects.create(name='Missing411', creation_date=timezone.now())
         Post.objects.create(
             title='post 1', 
             body='this is postbody1', 
             upvotes=0, 
             downvotes=0, 
             user=self.user, 
-            subreddit=self.subreddit
+            subreddit=self.subreddit,
+            posted_at=timezone.now()
         )
         Post.objects.create(
             title='post 2', 
@@ -24,7 +25,8 @@ class SubredditTestCase(TestCase):
             upvotes=0, 
             downvotes=0, 
             user=self.user, 
-            subreddit=self.subreddit
+            subreddit=self.subreddit,
+            posted_at=timezone.now()
         )
         Post.objects.create(
             title='post 3', 
@@ -32,10 +34,14 @@ class SubredditTestCase(TestCase):
             upvotes=0, 
             downvotes=0, 
             user=self.user, 
-            subreddit=self.subreddit
+            subreddit=self.subreddit,
+            posted_at=timezone.now()
         )
     
     def test_sort_by_new_should_return_posts_created_within_24_hours(self):
         sr = Subreddit.objects.get(name='Missing411')
+        posts = sr.sort_by_new()
         
+        
+
 
