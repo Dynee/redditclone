4109fb5014efc4b1d33a9c658a72008438976033
Dynee <lucaspadden@gmail.com>
2021-02-07 15:18:05 -0500

    created models for Subreddit, Post, Comment and generated migrations

--- subreddit/models.py
+++ subreddit/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Subreddit(models.Model):
+    name = models.CharField(max_length=25)
+    user_id = models.ManyToManyField(User)
+
+class Post(models.Model):
+    title = models.CharField(max_length=120)
+    body = models.TextField()
+    upvotes = models.IntegerField()
+    downvotes = models.IntegerField()
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    subreddit_id = models.ForeignKey(Subreddit, on_delete=models.CASCADE, null=True)
+
+class Comment(models.Model):
+    comment_text = models.TextField()
+    upvotes = models.IntegerField()
+    downvotes = models.IntegerField()
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE, null=True)
+    
+    
