01aaf0866fe223abfe4c5f6f644dd03d3bce627c
Dynee <lucaspadden@gmail.com>
2021-02-07 14:27:01 -0500

    created subreddit app and hooked it up to main project

--- redditclone/settings.py
+++ redditclone/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'subreddit.apps.SubredditConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
