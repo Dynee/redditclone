bed3b3f7c63f0b7107c2cb29e808848cfff3a1e8
Dynee <lucaspadden@gmail.com>
2021-02-08 14:04:44 -0500

    created template directory, and added view for index page

--- redditclone/settings.py
+++ redditclone/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'redditclone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['subreddit'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
